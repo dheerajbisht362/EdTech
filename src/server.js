29fda730eac2c6ada44d08a04c1db4c6b0a445ac
Deevanshu <deevanshu1997@gmail.com>
2021-09-04 23:05:55 +0530

    login page added

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const path = require("path");
 
 const connect = require("./configs/db");
 
+const homeController = require("./controllers/home.controller");
 const userController = require("./controllers/user.controller");
 
 const app = express();
@@ -18,6 +19,7 @@ app.set("views", path.join(__dirname, "views/"));
 app.set("view engine", "ejs");
 
 app.use("/user", userController);
+app.use("/", homeController);
 
 app.listen(3000, async () => {
   await connect();
